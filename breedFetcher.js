836608a0807fe8194f7374ab0f00e3c2a457720d
Vanessa Power <vanessa.l.power@gmail.com>
2022-09-06 21:23:45 -0700

    updating the error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,6 +4,10 @@ const breed = process.argv[2];
 request(
   `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
   (error, response, body) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
     const data = JSON.parse(body);
     if (data.length === 0) {
       console.log("No catz here!");
