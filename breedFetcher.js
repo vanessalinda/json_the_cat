349cc253d188547c06e5f6afc97595c9689320ae
Vanessa Power <vanessa.l.power@gmail.com>
2022-09-06 21:13:16 -0700

    fetching the catz description

---
+++ breedFetcher.js
@@ -0,0 +1,16 @@
+const request = require("request");
+const breed = process.argv[2];
+
+request(
+  `https://api.thecatapi.com/v1/breeds/search?q=${breed}`,
+  (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("No catz here!");
+    } else if (response.statusCode === 404) {
+      console.log(data.message);
+    } else {
+      console.log(data[0].description);
+    }
+  }
+);
